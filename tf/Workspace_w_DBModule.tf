ca199b4e54a4d8f38b890030a5ca886f195a6adb
David Leiva <david.leiva04@gmail.com>
2024-02-13 17:21:40 -0600

    first commit

---
+++ tf/Workspace_w_DBModule.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+}
+
+provider "databricks" {
+  host                   = "https://accounts.gcp.databricks.com"
+  google_service_account = var.google_service_account_email
+}
+
+resource "databricks_user" "gcp-user" {
+  user_name = "gcp-actions@databricks.com"
+}
+
+# module "examples_gcp-workspace-byovpc" {
+#   source                = "databricks/examples/databricks//modules/gcp-workspace-byovpc"
+#   version               = "0.2.16"
+#   databricks_account_id = ""
+#   delegate_from         = []
+#   google_project        = ""
+#   google_region         = ""
+#   prefix                = ""
+#   nat_name              = ""
+#   router_name           = ""
+#   subnet_name           = ""
+#   subnet_ip_cidr_range  = ""
+#   pod_ip_cidr_range     = ""
+#   svc_ip_cidr_range     = ""
+
+#   providers = {
+#     databricks = databricks,
+#     google     = google
+#   }
+# }
+
+# assign a metastore
---
+++ tf/variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  type    = string
+  default = "gcpdemo"
+}
+
+variable "project" {}
+
+
+variable "google_service_account_email" {
+
+}
