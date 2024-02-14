1c9a67a3b72f5fb9c5b1e54dbf7d6f34dff97a17
David Leiva <david.leiva04@gmail.com>
2024-02-13 23:05:59 -0600

    deploy

--- terraform/workspace.tf
+++ terraform/workspace.tf
@@ -32,8 +32,3 @@ resource "databricks_mws_workspaces" "this" {
 output "databricks_host" {
   value = databricks_mws_workspaces.this.workspace_url
 }
-
-output "databricks_token" {
-  value     = databricks_mws_workspaces.this.token[0].token_value
-  sensitive = true
-}
